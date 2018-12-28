f0d682ff013c4ec85c58ee3412c1f03b787a08ff
Sam Reaves <sreaves@kabbage.com>
2018-12-28 10:36:21 -0500

    Initial commit. Connections already made between question/answer labels and nextQuestion / showAnswer buttons to view controller

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        print("Show next question tapped")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        print("Show next question tapped")
     }
-
-
 }
 
